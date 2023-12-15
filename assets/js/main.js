617792014c9753bec38f43c799f54831c36d9541
VANN YA <99462479+VannYa1212@users.noreply.github.com>
2023-12-15 08:35:18 +0700

    Add button animation and back-to-top functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,37 +12,12 @@ function loading() {
 }
 loading();
 
-// back to top
-const backToTop = document.querySelector(".back-to-top");
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    backToTop.classList.add("active");
-  } else {
-    backToTop.classList.remove("active");
-  }
-});
-
-// click to scroll top
-backToTop.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-});
 
 // 2. navbar toggle button add class active to id #navbar-toggle then remove it after 2s
 
-const video1 = document.getElementById("video1");
-const playBtn1 = document.getElementById("playBtn1");
-const videoplay = () => {
-  if (video1.paused) {
-    video1.play();
-    playBtn1.innerHTML = '<i class="fa-regular fa-circle-pause"></i>';
-  } else {
-    video1.pause();
-    playBtn1.innerHTML = '<i class="fa-regular fa-circle-play"></i>';
-  }
-}
-videoplay();
 
 
+// button style
 
 
 
